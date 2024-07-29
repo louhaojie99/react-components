267c518de6c77d701a8c19dc30a59eea4470c923
louhaojie99 <851681631@qq.com>
2024-07-29 22:42:03 +0800

    feat: 部署到 gitpages 的 dumi 基础配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,4 +5,14 @@ export default defineConfig({
   themeConfig: {
     name: '@louhaojie99/react-components',
   },
+  locales: [
+    { id: 'zh-CN', name: '中文' },
+    { id: 'en-US', name: 'English' },
+  ],
+  /**
+   * 部署到 GitHubPage 的一些配置
+   * 配置可见: https://d.umijs.org/guide/faq#%E9%9D%9E%E6%A0%B9%E7%9B%AE%E5%BD%95%E9%83%A8%E7%BD%B2
+   */
+  base: '/react-components/',
+  publicPath: '/react-components/',
 });
